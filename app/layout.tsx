a80324ca445b7e390dc231f14ba345383dd00756
Gareth Davies <garethmd@googlemail.com>
2025-03-14 23:06:55 +0000

    add segment tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import { CompanyFooter } from "@/components/company-footer"
-import { CompanyHeader } from "@/components/company-header"
-import { ThemeProvider } from "@/components/theme-provider"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import type React from "react"
-import "./globals.css"
+import { CompanyFooter } from "@/components/company-footer";
+import { CompanyHeader } from "@/components/company-header";
+import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Script from "next/script";
+import type React from "react";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,6 +21,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Segment Analytics Script */}
+        <Script
+          id="segment-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(){var i="analytics",analytics=window[i]=window[i]||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","screen","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware","register"];analytics.factory=function(e){return function(){if(window[i].initialized)return window[i][e].apply(window[i],arguments);var n=Array.prototype.slice.call(arguments);if(["track","screen","alias","group","page","identify"].indexOf(e)>-1){var c=document.querySelector("link[rel='canonical']");n.push({__t:"bpc",c:c&&c.getAttribute("href")||void 0,p:location.pathname,u:location.href,s:location.search,t:document.title,r:document.referrer})}n.unshift(e);analytics.push(n);return analytics}};for(var n=0;n<analytics.methods.length;n++){var key=analytics.methods[n];analytics[key]=analytics.factory(key)}analytics.load=function(key,n){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.setAttribute("data-global-segment-analytics-key",i);t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(t,r);analytics._loadOptions=n};analytics._writeKey="D5RGyDGzm3CkOI6QaH7G73qfvJHWzh83";;analytics.SNIPPET_VERSION="5.2.0";
+            analytics.load("D5RGyDGzm3CkOI6QaH7G73qfvJHWzh83");
+            analytics.page();
+            }}();
+          `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="relative flex min-h-screen flex-col">
